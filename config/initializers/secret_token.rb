f53efc0ba1c18017f9c75d2d0a69d6aea3c2825d
App Academy Student <student@appacademy.io>
2014-01-15 10:43:39 -0500

    Initial commit and adds migration Users table

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+URLShortener::Application.config.secret_token = 'cede771173e93cae8d7a8389bc7ca2094ebe87508b6e31ff42cffb63c6dbf97f52e6586da3de4c8f633d990f0ae96d9a409b391ba952fcd00b66a6ca2a73206c'
